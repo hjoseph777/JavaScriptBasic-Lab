24a08d6a7c54534167b091c300de4b45abc14ebc
H Joseph <hjoseph777@gmail.com>
2024-09-19 16:04:43 -0400

    main

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# JavaScript Basics Lab
+
+## Synopsis
+
+This exercise demonstrates basic JavaScript concepts including:
+
+- Declaring and using variables
+- Logging output to the console
+- Demonstrating different data types (string, number, boolean)
+- Working with arrays and objects
+- Using basic arithmetic operators
+
+The script provides examples and logs the results to the console for easy understanding.
+
+## Author
+
+**Harry Joseph**
+
+## Date
+
+September 19, 2024
---
+++ script.js
@@ -0,0 +1,63 @@
+// Variables
+let studentName = "Harry";
+let studentAge = 18;
+let isEnrolled = true;
+
+//logs to access the console open the browser instance where your script is running and press F12 click on the console tab
+//this will print the variables to the console you may need to refresh the page to see the changes
+
+//variables
+
+console.log("demonstrating variables");
+console.log("Student name", studentName);
+console.log("Student age", studentAge);
+console.log("Enrolled", isEnrolled);
+
+//Demonstrate Data Types
+
+console.log("Demonstrating Data Types:");
+let stringType = "This is a string";
+console.log("String type:", stringType);
+let numberType = 12;
+console.log("Number type:", numberType);
+let booleanType = true;
+console.log("Boolean type:", booleanType);
+
+//Demonstrate arrays
+console.log("Demonstrating Arrays:");
+let arrayTypeFruit = ["apple", "banana", "cherry"];
+console.log("Array of fruits:", arrayTypeFruit);
+
+
+//Demonstrate objects
+console.log("Demonstrating Objects:");
+let objectTypeName = { firstName: "Harrisson", lastName: "Joseph" };
+console.log("Object type:", objectTypeName);
+
+
+// Use Basic Operators
+console.log("Demonstrating Basic Operators:");
+let number1 = 12;
+let number2 = 3 ;
+console.log("Addition:", number1 + number2);
+console.log("Subtraction:", number1 - number2);
+console.log("Multiplication:", number1 * number2);
+console.log("Division:", number1 / number2);
+
+
+// Comparison Operators
+console.log("Demonstrating Comparison Operators:");
+console.log("Greater than:", number1 > number2);
+console.log("Less than:", number1 < number2);
+console.log("Greater than or equal to:", number1 >= number2);
+console.log("Less than or equal to:", number1 <= number2);
+console.log("Equal to:", number1 == number2);
+console.log("Not equal to:", number1 != number2);
+
+// Logical Operators
+console.log("Demonstrating Logical Operators:");
+let logical1 = true;
+let logical2 = false;
+console.log("AND operator:", logical1 && logical2);
+console.log("OR operator:", logical1 || logical2);
+console.log("NOT operator:", !logical1);
